eaa6298f318b0d0bc6add74d273b67fcf533c944
Stephan Dowless <Stephan@Stephans-MacBook-Pro-2.local>
2017-08-07 02:34:23 -0400

    Initial Commit

---
+++ DreamList/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  DreamList
+//
+//  Created by Stephan Dowless on 1/6/17.
+//  Copyright © 2017 Stephan Dowless. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DreamList/materialView.swift
@@ -0,0 +1,9 @@
+//
+//  materialView.swift
+//  DreamList
+//
+//  Created by Stephan Dowless on 1/6/17.
+//  Copyright © 2017 Stephan Dowless. All rights reserved.
+//
+
+import Foundation
